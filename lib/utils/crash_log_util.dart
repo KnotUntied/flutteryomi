dca0765e5617e5a7397e83cfe26394897750b6e0
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2023-10-10 12:40:58 +0800

    Progress on settings page

---
+++ lib/utils/crash_log_util.dart
@@ -0,0 +1,18 @@
+import 'dart:io' show Platform;
+
+class CrashLogUtil {
+  String getDebugInfo() {
+    return "";
+    //return """
+    //  App version: ${BuildConfig.VERSION_NAME} (${BuildConfig.FLAVOR}, ${BuildConfig.COMMIT_SHA}, ${BuildConfig.VERSION_CODE}, ${BuildConfig.BUILD_TIME})
+    //  Android version: ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT})
+    //  Android build ID: ${Build.DISPLAY}
+    //  Device brand: ${Build.BRAND}
+    //  Device manufacturer: ${Build.MANUFACTURER}
+    //  Device name: ${Build.DEVICE}
+    //  Device model: ${Build.MODEL}
+    //  Device product name: ${Build.PRODUCT}
+    //  WebView user agent: ${WebViewUtil.getInferredUserAgent(context)}
+    //""".trimIndent()
+  }
+}
