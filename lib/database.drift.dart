ade5f5cf6e09618064351c468d97c88599e1c622
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2023-11-02 20:26:30 +0800

    Fix wrong updatesView

--- lib/database.drift.dart
+++ lib/database.drift.dart
@@ -2,10 +2,10 @@
 import 'package:drift/drift.dart' as i0;
 import 'package:flutteryomi/models/tables/mangas.drift.dart' as i1;
 import 'package:flutteryomi/models/tables/chapters.drift.dart' as i2;
-import 'package:flutteryomi/models/tables/history.drift.dart' as i3;
-import 'package:flutteryomi/models/tables/categories.drift.dart' as i4;
-import 'package:flutteryomi/models/tables/mangas_categories.drift.dart' as i5;
-import 'package:flutteryomi/models/views/updatesView.drift.dart' as i6;
+import 'package:flutteryomi/models/views/updatesView.drift.dart' as i3;
+import 'package:flutteryomi/models/tables/history.drift.dart' as i4;
+import 'package:flutteryomi/models/tables/categories.drift.dart' as i5;
+import 'package:flutteryomi/models/tables/mangas_categories.drift.dart' as i6;
 import 'package:flutteryomi/models/views/libraryView.drift.dart' as i7;
 import 'package:flutteryomi/models/views/historyView.drift.dart' as i8;
 import 'package:flutteryomi/models/tables/sources.drift.dart' as i9;
@@ -16,35 +16,35 @@ abstract class $AppDatabase extends i0.GeneratedDatabase {
   $AppDatabase(i0.QueryExecutor e) : super(e);
   late final i1.Mangas mangas = i1.Mangas(this);
   late final i2.Chapters chapters = i2.Chapters(this);
-  late final i3.History history = i3.History(this);
-  late final i4.Categories categories = i4.Categories(this);
-  late final i5.MangasCategories mangasCategories = i5.MangasCategories(this);
-  late final i6.LibraryView libraryView = i6.LibraryView(this);
+  late final i3.UpdatesView updatesView = i3.UpdatesView(this);
+  late final i4.History history = i4.History(this);
+  late final i5.Categories categories = i5.Categories(this);
+  late final i6.MangasCategories mangasCategories = i6.MangasCategories(this);
   late final i7.LibraryView libraryView = i7.LibraryView(this);
   late final i8.HistoryView historyView = i8.HistoryView(this);
   late final i9.Sources sources = i9.Sources(this);
   late final i10.MangaSync mangaSync = i10.MangaSync(this);
-  i4.CategoriesDrift get categoriesDrift => i11.ReadDatabaseContainer(this)
-      .accessor<i4.CategoriesDrift>(i4.CategoriesDrift.new);
+  i5.CategoriesDrift get categoriesDrift => i11.ReadDatabaseContainer(this)
+      .accessor<i5.CategoriesDrift>(i5.CategoriesDrift.new);
   i2.ChaptersDrift get chaptersDrift => i11.ReadDatabaseContainer(this)
       .accessor<i2.ChaptersDrift>(i2.ChaptersDrift.new);
-  i3.HistoryDrift get historyDrift => i11.ReadDatabaseContainer(this)
-      .accessor<i3.HistoryDrift>(i3.HistoryDrift.new);
+  i4.HistoryDrift get historyDrift => i11.ReadDatabaseContainer(this)
+      .accessor<i4.HistoryDrift>(i4.HistoryDrift.new);
   i10.MangaSyncDrift get mangaSyncDrift => i11.ReadDatabaseContainer(this)
       .accessor<i10.MangaSyncDrift>(i10.MangaSyncDrift.new);
   i1.MangasDrift get mangasDrift => i11.ReadDatabaseContainer(this)
       .accessor<i1.MangasDrift>(i1.MangasDrift.new);
-  i5.MangasCategoriesDrift get mangasCategoriesDrift =>
+  i6.MangasCategoriesDrift get mangasCategoriesDrift =>
       i11.ReadDatabaseContainer(this)
-          .accessor<i5.MangasCategoriesDrift>(i5.MangasCategoriesDrift.new);
+          .accessor<i6.MangasCategoriesDrift>(i6.MangasCategoriesDrift.new);
   i9.SourcesDrift get sourcesDrift => i11.ReadDatabaseContainer(this)
       .accessor<i9.SourcesDrift>(i9.SourcesDrift.new);
   i8.HistoryViewDrift get historyViewDrift => i11.ReadDatabaseContainer(this)
       .accessor<i8.HistoryViewDrift>(i8.HistoryViewDrift.new);
   i7.LibraryViewDrift get libraryViewDrift => i11.ReadDatabaseContainer(this)
       .accessor<i7.LibraryViewDrift>(i7.LibraryViewDrift.new);
-  i6.UpdatesViewDrift get updatesViewDrift => i11.ReadDatabaseContainer(this)
-      .accessor<i6.UpdatesViewDrift>(i6.UpdatesViewDrift.new);
+  i3.UpdatesViewDrift get updatesViewDrift => i11.ReadDatabaseContainer(this)
+      .accessor<i3.UpdatesViewDrift>(i3.UpdatesViewDrift.new);
   @override
   Iterable<i0.TableInfo<i0.Table, Object?>> get allTables =>
       allSchemaEntities.whereType<i0.TableInfo<i0.Table, Object?>>();
@@ -52,24 +52,24 @@ abstract class $AppDatabase extends i0.GeneratedDatabase {
   List<i0.DatabaseSchemaEntity> get allSchemaEntities => [
         mangas,
         chapters,
+        updatesView,
         history,
         categories,
         mangasCategories,
         libraryView,
-        libraryView,
         historyView,
         sources,
-        i5.updateLastModifiedAtMangasCategories,
+        i6.updateLastModifiedAtMangasCategories,
         i1.libraryFavoriteIndex,
         i1.mangasUrlIndex,
         i1.updateLastFavoritedAtMangas,
         i1.updateLastModifiedAtMangas,
         mangaSync,
-        i3.historyHistoryChapterIdIndex,
+        i4.historyHistoryChapterIdIndex,
         i2.chaptersMangaIdIndex,
         i2.chaptersUnreadByMangaIndex,
         i2.updateLastModifiedAtChapters,
-        i4.systemCategoryDeleteTrigger
+        i5.systemCategoryDeleteTrigger
       ];
   @override
   i0.StreamQueryUpdateRules get streamUpdateRules =>
--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-//import 'package:flutteryomi/utils/crash_log_util.dart';
+//import 'package:flutteryomi/utils/crash_log.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
