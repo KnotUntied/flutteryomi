5f7306a1ef510579bffc19b427843a491fefc9b0
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2024-02-05 12:39:40 +0800

    List pref widget

---
+++ lib/presentation/more/settings/widget/list_preference_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:flutteryomi/presentation/more/settings/widget/text_preference_widget.dart';
+
+class ListPreferenceWidget<T> extends StatelessWidget {
+  const ListPreferenceWidget({
+    super.key,
+    required this.value,
+    required this.title,
+    this.subtitle,
+    this.icon,
+    required this.entries,
+    required this.onValueChange,
+  });
+
+  final T value;
+  final String title;
+  final String? subtitle;
+  final IconData? icon;
+  final Map<T, String> entries;
+  final ValueChanged<T> onValueChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextPreferenceWidget(
+      title: title,
+      subtitle: subtitle,
+      icon: icon,
+      onPreferenceClick: () => showAdaptiveDialog(
+        context: context,
+        builder: (context) => _ListPreferenceDialog(
+          title: title,
+          value: value,
+          entries: entries,
+          onValueChange: onValueChange,
+        ),
+      ),
+    );
+  }
+}
+
+class _ListPreferenceDialog<T> extends StatelessWidget {
+  const _ListPreferenceDialog({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.entries,
+    required this.onValueChange,
+  });
+
+  final String title;
+  final T value;
+  final Map<T, String> entries;
+  final ValueChanged<T> onValueChange;
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = AppLocalizations.of(context);
+    final entriesList = entries.entries.toList();
+    return AlertDialog.adaptive(
+      title: Text(title),
+      content: ListView.builder(
+        itemCount: entries.length,
+        itemBuilder: (context, index) {
+          final current = entriesList[index];
+          return RadioListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text(current.value),
+            value: current.key,
+            groupValue: value,
+            onChanged: (_) {
+              Navigator.of(context).pop();
+              onValueChange(current.key);
+            },
+          );
+        },
+      ),
+      actions: [
+        TextButton(
+          child: Text(lang.action_cancel),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ],
+    );
+  }
+}
