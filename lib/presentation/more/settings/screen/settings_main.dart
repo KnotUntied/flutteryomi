ec0287d3d8b51619f39fef0d53cc25545f98aa4d
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2024-05-05 14:40:49 +0800

    Settings main screen

--- lib/presentation/more/settings/screen/settings_main.dart
+++ lib/presentation/more/settings/screen/settings_main.dart
@@ -4,6 +4,16 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'package:flutteryomi/presentation/components/app_bar.dart';
 import 'package:flutteryomi/presentation/more/settings/screen/about/about.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_advanced.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_appearance.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_browse.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_data.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_download.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_library.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_reader.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_search.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_security.dart';
+import 'package:flutteryomi/presentation/more/settings/screen/settings_tracking.dart';
 import 'package:flutteryomi/presentation/more/settings/widget/text_preference_widget.dart';
 
 part 'settings_main.freezed.dart';
@@ -23,60 +33,60 @@ class _SettingsMainScreenState extends State<SettingsMainScreen> {
     final lang = AppLocalizations.of(context);
     //final containerColor = twoPane ? _getPalerSurface(context) : Theme.of(context).colorScheme.surface;
     final items = [
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_appearance,
-      //  subtitle: lang.pref_appearance_summary,
-      //  iconData: Icons.palette_outlined,
-      //  screen: SettingsAppearanceScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_library,
-      //  subtitle: lang.pref_library_summary,
-      //  iconData: Icons.collections_bookmark_outlined,
-      //  screen: SettingsLibraryScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_reader,
-      //  subtitle: lang.pref_reader_summary,
-      //  iconData: Icons.chrome_reader_mode_outlined,
-      //  screen: SettingsReaderScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_downloads,
-      //  subtitle: lang.pref_downloads_summary,
-      //  iconData: Icons.get_app_outlined,
-      //  screen: SettingsDownloadScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_tracking,
-      //  subtitle: lang.pref_tracking_summary,
-      //  iconData: Icons.sync_outlined,
-      //  screen: SettingsTrackingScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.browse,
-      //  subtitle: lang.pref_browse_summary,
-      //  iconData: Icons.explore_outlined,
-      //  screen: SettingsBrowseScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.label_data_storage,
-      //  subtitle: lang.pref_backup_summary,
-      //  iconData: Icons.storage_outlined,
-      //  screen: SettingsDataScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_security,
-      //  subtitle: lang.pref_security_summary,
-      //  iconData: Icons.security_outlined,
-      //  screen: SettingsSecurityScreen(),
-      //),
-      //SettingsMainScreenItem(
-      //  title: lang.pref_category_advanced,
-      //  subtitle: lang.pref_advanced_summary,
-      //  iconData: Icons.code_outlined,
-      //  screen: SettingsAdvancedScreen(),
-      //),
+      SettingsMainScreenItem(
+        title: lang.pref_category_appearance,
+        subtitle: lang.pref_appearance_summary,
+        iconData: Icons.palette_outlined,
+        screen: const SettingsAppearanceScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_library,
+        subtitle: lang.pref_library_summary,
+        iconData: Icons.collections_bookmark_outlined,
+        screen: const SettingsLibraryScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_reader,
+        subtitle: lang.pref_reader_summary,
+        iconData: Icons.chrome_reader_mode_outlined,
+        screen: const SettingsReaderScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_downloads,
+        subtitle: lang.pref_downloads_summary,
+        iconData: Icons.get_app_outlined,
+        screen: const SettingsDownloadScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_tracking,
+        subtitle: lang.pref_tracking_summary,
+        iconData: Icons.sync_outlined,
+        screen: const SettingsTrackingScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.browse,
+        subtitle: lang.pref_browse_summary,
+        iconData: Icons.explore_outlined,
+        screen: const SettingsBrowseScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.label_data_storage,
+        subtitle: lang.pref_backup_summary,
+        iconData: Icons.storage_outlined,
+        screen: const SettingsDataScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_security,
+        subtitle: lang.pref_security_summary,
+        iconData: Icons.security_outlined,
+        screen: const SettingsSecurityScreen(),
+      ),
+      SettingsMainScreenItem(
+        title: lang.pref_category_advanced,
+        subtitle: lang.pref_advanced_summary,
+        iconData: Icons.code_outlined,
+        screen: const SettingsAdvancedScreen(),
+      ),
       SettingsMainScreenItem(
         title: lang.pref_category_about,
         subtitle:
@@ -95,13 +105,12 @@ class _SettingsMainScreenState extends State<SettingsMainScreen> {
             title: lang.action_search,
             iconData: Icons.search_outlined,
             onClick: () {
-              //TODO
-              //Navigator.push(
-              //  context,
-              //  MaterialPageRoute(
-              //    builder: (context) => const SettingsSearchScreen(),
-              //  ),
-              //);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsSearchScreen(),
+                ),
+              );
             },
           ),
         ],
