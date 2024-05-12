15e1b0c8cbe2ce34a1710785b91b0df0d23a4edf
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2024-05-12 18:46:57 +0800

    Progress on onboarding screen

--- lib/presentation/more/onboarding/onboarding.dart
+++ lib/presentation/more/onboarding/onboarding.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class OnboardingScreen extends StatefulWidget {
+import 'package:flutteryomi/domain/base/base_preferences.dart';
+import 'package:flutteryomi/presentation/components/material/constants.dart';
+import 'package:flutteryomi/presentation/screens/info_screen.dart';
+
+//TODO
+class OnboardingScreen extends ConsumerStatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
+  ConsumerState<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
-class _OnboardingScreenState extends State<OnboardingScreen> {
+class _OnboardingScreenState extends ConsumerState<OnboardingScreen> {
+  final steps = [
+    //ThemeStep(),
+    //StorageStep(),
+    //PermissionStep(),
+    //GuidesStep(onRestoreBackup: onRestoreBackup),
+  ];
+  int currentStep = 0;
+
   @override
   Widget build(BuildContext context) {
-    //TODO
-    return const Placeholder();
+    final lang = AppLocalizations.of(context);
+    final isLastStep = currentStep == steps.length - 1;
+    final basePreferences = ref.watch(basePreferencesProvider);
+    final shownOnboardingStream =
+        basePreferences.shownOnboardingFlow().changes();
+    return StreamBuilder<bool>(
+      stream: shownOnboardingStream,
+      builder: (context, snapshot) {
+        final canPop = snapshot.data ?? false;
+        return PopScope(
+          canPop: canPop,
+          child: InfoScreen(
+            icon: Icons.rocket_launch_outlined,
+            headingText: lang.onboarding_heading,
+            subtitleText: lang.onboarding_description,
+            acceptText: isLastStep
+                ? lang.onboarding_action_finish
+                : lang.onboarding_action_next,
+            onAcceptClick: steps[currentStep].isComplete
+                ? () {
+                    if (isLastStep) {
+                      Navigator.pop(context);
+                      basePreferences.shownOnboardingFlow().set(true);
+                    } else {
+                      setState(() => currentStep++);
+                    }
+                  }
+                : null,
+            children: [
+              Container(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                padding: const EdgeInsets.symmetric(
+                  vertical: MaterialPadding.small,
+                ),
+                child: steps[currentStep],
+              ),
+            ],
+          ),
+        );
+      }
+    );
   }
 }
